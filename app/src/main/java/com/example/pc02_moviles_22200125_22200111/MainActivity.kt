bd166d0350953490ad3e89b9ffd4aeb5e36b1e94
csMattias09 <22200125@ue.edu.pe>
2025-11-22 10:26:45 -0500

    feat: Integrate Firebase
    Adds the Google Services plugin and Firebase Bill of Materials (BOM) to the project. Includes the `google-services.json` configuration file to connect the application with Firebase services.

--- app/src/main/java/com/example/pc02_moviles_22200125_22200111/MainActivity.kt
+++ app/src/main/java/com/example/pc02_moviles_22200125_22200111/MainActivity.kt
@@ -3,45 +3,28 @@ package com.example.pc02_moviles_22200125_22200111
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.pc02_moviles_22200125_22200111.navigation.AppNavGraph
 import com.example.pc02_moviles_22200125_22200111.ui.theme.PC02_MOVILES_2220012522200111Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint // <-- ¡Añadido para Hilt!
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PC02_MOVILES_2220012522200111Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Llama a tu grafo de navegación aquí
+                    AppNavGraph()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PC02_MOVILES_2220012522200111Theme {
-        Greeting("Android")
     }
 }
